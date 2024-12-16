d1888cf8cefe74a2e11498cb15e7edb8d12a80db
furkan eriç <furkaneric9@gmail.com>
2024-12-16 17:44:12 +0300

    genelbakıscontroller düzenledi ana sayfa düzenlendi

--- TaskAssignment/Controllers/GenelBakısController.cs
+++ TaskAssignment/Controllers/GenelBakısController.cs
@@ -3,14 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TaskAssignment.Models.DateContext;
 
 namespace TaskAssignment.Controllers
 {
     public class GenelBakısController : Controller
     {
+        private ProjeTakipDBContext db = new ProjeTakipDBContext();
         // GET: GenelBakıs
         public ActionResult Index()
         {
+            int projesayisi =db.PersonelProjeleri.Count();
+            ViewBag.Projesayisi = projesayisi;
+
+
+
+
+            int tamamlanmisProje=db.PersonelProjeleri.Where(p=>p.TamamlanmaDurumu==true).Count();
+            ViewBag.TamamlanmisProje=tamamlanmisProje;
+
+
+
+            var yuksekoncelikliProjeler = db.PersonelProjeleri.Where(p => p.OncelıkDurumu == "Yüksek Öncelikli").Count();
+            ViewBag.YüksekOncelikliProje=yuksekoncelikliProjeler;
+
+            var dusukoncelikliProje = db.PersonelProjeleri.Where(p => p.OncelıkDurumu == "Düşük Öncelikli").Count();
+            ViewBag.DusukOncelikliProje = dusukoncelikliProje;
+
+
+
+            var ortaoncelikliProje = db.PersonelProjeleri.Where(p => p.OncelıkDurumu == "Orta Öncelikli").Count();
+            ViewBag.OrtaOncelikliProje = ortaoncelikliProje;
+
+
+
+
+
+
             return View();
         }
     }
